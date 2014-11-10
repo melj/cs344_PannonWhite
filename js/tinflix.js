99ee9426aefd9e309510a610f9f3146ddef59d9a
Melissa Johnson <d4u7@ugrad.cs.ubc.ca>
2014-11-10 04:01:05 -0800

    Added a few things. Still very rough

--- js/tinflix.js
+++ js/tinflix.js
@@ -1,1 +1,21 @@
 
+
+$(window).load(function() {	
+	$(".details-link").click(function(e) {
+		$(".course-side").toggle();
+		$(".details-link").toggle();
+	});	
+ 
+	$(".course-option").click(function(e) {
+
+	});
+
+	$(".tf-queue-nav").click(function(e) {		
+
+	});
+
+	$(".tf-queue-option").click(function(e) {		
+		$(".tf-queue-option").removeClass("selected");
+		$(e.target).addClass("selected");
+	});
+});
